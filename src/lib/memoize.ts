3fc2dd02aec4824ba199a4de5c6d9b562673355f
Botao Pan <botao_pan@gtfund.com>
2021-02-01 14:28:18 +0800

    memoize: set createdAt after execution resolves

--- src/lib/memoize.ts
+++ src/lib/memoize.ts
@@ -10,27 +10,33 @@ export default function(
         if (args.length) throw new Error(`memoization with args not supported`);
 
         type Cache = {
-            value: any,
-            createdAt: Date,
+            value?: any,
+            promise?: Promise<any>,
+            createdAt?: Date,
         };
 
         if (Reflect.hasMetadata(cacheSymbol, target[method])) {
             let v: Cache = Reflect.getMetadata(cacheSymbol, target[method]);
+            if (v.promise) return await v.promise;
             if (expired(v.createdAt)) return await renew();
             else return v.value;
         } else return await renew();
 
         async function renew() {
-            let value = await execution();
-            let cache: Cache = { value, createdAt: new Date() };
+            let promise = execution();
+            let cache: Cache = { promise };
             Reflect.defineMetadata(cacheSymbol, cache, target[method]);
-            return value
+            cache.value = await cache.promise;
+            cache.createdAt = new Date();
+            delete cache.promise;
+            return cache.value
         };
     }
 
     return pointcut;
 
-    function expired(d: Date) {
+    function expired(d: Date | undefined) {
+        if (!d) return true;
         return new Date().getTime() - d.getTime() > timeToLiveInSeconds * 1000
     }
 }
